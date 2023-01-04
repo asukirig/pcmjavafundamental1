95d92a1c8f6386bb96a1d6772caa077bc0f022ee
asukirig <iqbalwibisono28@gmail.com>
2023-01-04 20:27:15 +0700

    first commit

---
+++ src/main/java/com/juaracoding/pcmjavafundamental/buatcommitjava.java
@@ -0,0 +1,17 @@
+package src.main.java.com.juaracoding.pcmjavafundamental;
+/*
+Created By IntelliJ IDEA 2022.2.3 (Community Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author elmeister a.k.a. Iqbal Wibisono
+Java Developer
+Created on 04/01/2023 20:26
+@Last Modified 04/01/2023 20:26
+Version 1.1
+*/
+
+public class buatcommitjava {
+
+    public static void main(String[] args) {
+        System.out.printf("buat commit ke git hub");
+    }
+}
